6b8d7607ca2f081c9af4acd073ff2da33b6f3ac8
EmilieS <simonard@intechinfo.fr>
2014-01-09 11:37:37 +0100

    debut.

---
+++ intechBook/core/ArreteRelation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace core
+{
+    class ArreteRelation
+    {
+        ArreteRelation(int orig, int dest, TypeRelation relation)
+        {
+            _orig = orig;
+            _dest = dest;
+            _relation = relation;
+        }
+
+        int _orig;
+        int _dest;
+
+        TypeRelation _relation;
+    }
+}
+
+enum  TypeRelation 
+{
+    AMI=1,
+    COLLEGUE=2
+};
---
+++ intechBook/core/SommetPersonne.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace core
+{
+    public class SommetPersonne
+    {
+        SommetPersonne(string Nom,string Prenom,string Mail)
+        {
+            Contacts = new List<ArreteRelation>();
+            _nom = Nom;
+            _prénom = Prenom;
+            _email = Mail;
+            _currnum++;
+            _num=_currnum;
+
+        }
+        List<ArreteRelation> Contacts;
+        readonly int _num;
+        string _nom;
+        string _prénom;
+        string _email;
+
+        static int _currnum;
+    }
+}
